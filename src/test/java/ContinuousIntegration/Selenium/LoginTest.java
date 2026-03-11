5500c0ef0f05ce0fb285ee0b64ac2e8afd798355
Jens Nordström <jens.nordstrom@gmail.com>
2026-03-11 17:47:21 +0100

    Update login test

--- src/test/java/ContinuousIntegration/Selenium/LoginTest.java
+++ src/test/java/ContinuousIntegration/Selenium/LoginTest.java
@@ -76,10 +76,11 @@ public class LoginTest {
         }
 
         String loginButtonText = loginButtonElement.getAttribute("value");
+
         if (!"Login".equals(loginButtonText)) {
             throw new AssertionError("Expected 'Login' but got '" + loginButtonText + "'");
         }
-        
+
         // Setup credentials for failed login
         String failedUsername = "wrong_user";
         String failedPassword = "no_secret_sauce";
